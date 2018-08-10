def932199b3ea5b0e6f8017820e74751c10890ec
“zhujianquan <786056735@qq.com>
2018-08-10 14:28:31 +0800

    Default

---
+++ src/main/java/com/action/UserAction.java
@@ -0,0 +1,15 @@
+package com.action;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class UserAction {
+
+
+    @RequestMapping
+    public Object getUserlist(){
+
+        return null;
+    }
+}
